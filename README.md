8dd199957c67551cb40c43f202998e845efab139
neuthral <tomi.kurvinen@gmail.com>
2025-06-11 14:49:46 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bus timetables
+
+display my daily busses timetables
+
+made with Tauri + Vanilla javascript
+
---
+++ src/main.js
@@ -0,0 +1,8 @@
+const { invoke } = window.__TAURI__.core;
+
+window.addEventListener("DOMContentLoaded", () => {
+  // location.replace("https://tremonitori.digitransit.fi/view?cont=enzGzVxV+2xqdsIGz7ru5A==")
+
+  //var frame = document.querySelector('iframe')
+
+});
